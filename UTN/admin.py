e08fee26b8cfedadf0e454b068ab6bcde499bec5
lucasgalleguillo <galleguillolucas2006@gmail.com>
2023-10-23 11:34:35 -0300

    modificaciones en el admin site

--- UTN/admin.py
+++ UTN/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
+# Register your models here.
 
-# Register your models here. modificaciones
+
+modules = [    
+    Tipodocumento,
+    Usuario,
+    Turno,
+    Carrera,
+    Materia,
+    Curso,
+    Correlatividad,
+    EstadoCurso,
+    Historial,
+    NotaEvaluacion,
+    Profesor,
+    Inscripcion,
+    Alumno
+]
+
+for i in modules:
+    admin.site.register(i)
--- UTN/views.py
+++ UTN/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def Inicio(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html')        
