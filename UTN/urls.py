c27d65178b8a0881535304e3dc99ec3d66bebaa0
lucasgalleguillo <galleguillolucas2006@gmail.com>
2023-11-03 12:07:09 -0300

    creacion de login y modificaciones de signin

--- UTN/urls.py
+++ UTN/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.inicio),
     path('login/', views.login),
-    path('singin/', views.singin)
+    path('signin/', views.signin)
 ]
--- UTN/views.py
+++ UTN/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from UTN.models import *
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+
 
 # Create your views here.
 def inicio(request):
     return render(request, 'index.html')        
 
-def login (request):
+def login (request):    
     if request.method == 'POST':
+        usuario = request.POST['usuario']
+        contrasena = request.POST['contrasena']
+
+        user = authenticate(request, username=usuario, password=contrasena)
+        if user is not None:
+            login(request, user)
+            
+            #request.session["user"] = usuario
+            #request.session["password"] = contrasena
+            return redirect('/')  
+        else:
+            return render(request, HttpResponse('logiate bien boludito'))
+    else:    
+        return render(request, 'login.html')
+
+def signin(request):
+    if request.method == 'POST':    
         nombre_completo = request.POST['nombre_completo']
         domicilio = request.POST['domicilio']
         mail = request.POST['mail']
@@ -24,8 +44,7 @@ def login (request):
             documento=documento,
             fecha_nacimiento=fecha_nacimiento
         )
+        return redirect('/')
     else:
-        return render(request, 'login.html')        
-
-def singin(request):
-    return render(request, 'singin.html')        
+        tiposdocumento = Tipodocumento.objects.all()
+        return render(request, 'signin.html', { 'tipos_documentos' : tiposdocumento})
