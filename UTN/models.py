62ed0cf8cc6b9711aa8e95ed59d174cd6169663f
lucasgalleguillo <galleguillolucas2006@gmail.com>
2023-11-22 17:37:32 -0300

    modificaciones en el models

--- UTN/models.py
+++ UTN/models.py
@@ -1,14 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 class Tipodocumento(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
 
-class Usuario(models.Model):
-    nombre_completo = models.CharField(max_length=255)
-    domicilio = models.CharField(max_length=255)
-    mail = models.EmailField()
-    telefono = models.CharField(max_length=20)
+class Usuario(AbstractUser):
     tipodocumento = models.ForeignKey(Tipodocumento, on_delete=models.CASCADE)
     documento = models.CharField(max_length=20)
     fecha_nacimiento = models.DateField()
@@ -31,40 +29,48 @@ class Curso(models.Model):
     nombreCurso = models.CharField(max_length=255)
     turno = models.ForeignKey(Turno, on_delete=models.CASCADE)
     nivel = models.IntegerField()
-    listaHorarios = models.TextField()
-
-class Correlatividad(models.Model):
-    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
-    estado_curso = models.ForeignKey('EstadoCurso', on_delete=models.CASCADE)
+    listaHorarios = models.CharField(max_length=255)
 
 class EstadoCurso(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
 
-class Historial(models.Model):
+
+class Correlatividad(models.Model):
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    estado_curso = models.ForeignKey(EstadoCurso, on_delete=models.CASCADE)
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=255, default='')
+    apellido = models.CharField(max_length=255, default='')
+    correo = models.EmailField(default='')
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+class CondicionFinal(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
-    fecha_inicio = models.DateField()
-    fecha_final = models.DateField()
-    estado_curso = models.ForeignKey(EstadoCurso, on_delete=models.CASCADE)
+
+class Inscripcion(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, default='')
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    condicionFinal = models.ForeignKey(CondicionFinal, on_delete=models.CASCADE)
+    fechaInicio = models.DateTimeField()
+    fechaFinal = models.DateTimeField()
 
 class NotaEvaluacion(models.Model):
     nombre = models.CharField(max_length=255)
     tipoEvaluacion = models.CharField(max_length=255)
     valor = models.IntegerField()
     fecha = models.DateField()
-    profesor = models.ForeignKey('Profesor', on_delete=models.CASCADE)
+    Inscripcion = models.ForeignKey(Inscripcion, on_delete=models.CASCADE, default='')
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
 
-class Profesor(models.Model):
-    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
-
-class Inscripcion(models.Model):
-    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
-    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
-    condicionFinal = models.ForeignKey(EstadoCurso, on_delete=models.CASCADE)
-    fechaInicio = models.DateTimeField()
-    fechaFinal = models.DateTimeField()
-
-class Alumno(Usuario):
-    listaCarrera = models.ManyToManyField(Carrera)
-    listaInscripciones = models.ManyToManyField(Inscripcion)
+class Historial(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    fecha_inicio = models.DateField()
+    fecha_final = models.DateField()
+    Inscripcion = models.ForeignKey(Inscripcion, on_delete=models.CASCADE, default='')
+    estado_curso = models.ForeignKey(EstadoCurso, on_delete=models.CASCADE)
